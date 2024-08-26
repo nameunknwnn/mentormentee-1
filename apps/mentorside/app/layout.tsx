ccad8347d7415b3b85a6f9d38389b78aed21c543
Aditya Rawat <adityarawat@Adityas-Laptop.local>
2024-08-26 16:04:19 +0530

    init

--- apps/mentorside/app/layout.tsx
+++ apps/mentorside/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Topbar } from "@repo/ui/topbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,6 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <div>
+          <Topbar></Topbar>
+        </div>
         {children}
       </body>
     </html>
---
+++ apps/mentorside/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+   <div className="text-2xl pt-20 px-10">
+     landing page
+   </div>
+  );
+}
